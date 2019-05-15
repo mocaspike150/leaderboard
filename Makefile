9378716510724e3856cb648d6ce3ab7f04d6f2e2
Sam Liu <ontouchstart@gmail.com>
2019-05-14 22:31:25 -0400

    make fetch convert

--- Makefile
+++ Makefile
@@ -1,18 +1,29 @@
-CMD=/usr/local/bin/node bin/fetch.js
+FETCH=/usr/local/bin/node bin/fetch.js
+CONVERT=/usr/local/bin/node bin/convert.js
+
+all: fetch convert
 
 fetch: 128445 72363 301632 523430 302045
 
 128445:
-	$(CMD) week01 128445
+	$(FETCH) week01 128445
 
 72363:
-	$(CMD) week01 72363
+	$(FETCH) week01 72363
 
 301632:
-	$(CMD) week01 301632
+	$(FETCH) week01 301632
 
 523430:
-	$(CMD) week01 523430
+	$(FETCH) week01 523430
 
 302045:
-	$(CMD) week01 302045
+	$(FETCH) week01 302045
+
+convert:
+	$(CONVERT) week01 128445
+	$(CONVERT) week01 72363
+	$(CONVERT) week01 301632
+	$(CONVERT) week01 523430
+	$(CONVERT) week01 302045
+
---
+++ bin/convert.js
@@ -0,0 +1,47 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const fs = require('fs');
+
+const convert = (week, id) => {
+  const base = `docs/${week}`;
+  const input = `${base}/${id}.html`;
+  const output = `${base}/${id}.json`;
+
+  JSDOM.fromFile(input, {}).then(dom => {
+    const table = dom.window.document.querySelector('table');
+    const html =  table.innerHTML;
+    const text =  table.textContent;
+    const lines = text.split('\n').filter((d) => (d.length > 0));
+
+    let data = [[], [], [], [], [], [], []];
+    let count = -6;
+    for (let line of lines) {
+      if(count > 0) {
+        for (let i of [1, 2, 3, 4, 5, 6]) {
+          if(count % 7 == i) {
+            data[i - 1].push(line);
+          }
+        }
+        if(count %7 == 0) {
+          data[6].push(line);
+        }
+      }
+      count++;
+    }
+
+    let leaderboard = [];
+
+    let c = 0;
+    for( let i in data[0] ) {
+      let tmp = [];
+      for( let j in [1, 2, 3, 4, 5, 6, 7] ) {
+        tmp.push(data[j][c]);
+      }
+      leaderboard.push(tmp);
+      c++;
+    }
+    fs.writeFile(output, JSON.stringify(leaderboard), (err) => { if (err) throw err; console.log(output); })
+  });
+};
+
+convert(process.argv[2], process.argv[3])
