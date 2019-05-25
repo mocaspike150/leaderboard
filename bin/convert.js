dd8bd6400cc220b16e84757cbb027875be07d345
Sam Liu <ontouchstart@gmail.com>
2019-05-24 23:53:41 -0400

    Update convert.js

--- bin/convert.js
+++ bin/convert.js
@@ -6,42 +6,43 @@ const convert = (id) => {
   const base = `data`;
   const input = `${base}/html/${id}.html`;
   const output = `${base}/json/${id}.json`;
+  const data = fs.readFileSync(input, 'utf8')
+  
+  const dom = new JSDOM(data);
+  const table = dom.window.document.querySelector('table');
+  const html =  table.innerHTML;
+  const text =  table.textContent;
+  const lines = text.split('\n').filter((d) => (d.length > 0));
 
-  JSDOM.fromFile(input, {}).then(dom => {
-    const table = dom.window.document.querySelector('table');
-    const html =  table.innerHTML;
-    const text =  table.textContent;
-    const lines = text.split('\n').filter((d) => (d.length > 0));
-
-    let data = [[], [], [], [], [], [], []];
-    let count = -6;
-    for (let line of lines) {
-      if(count > 0) {
-        for (let i of [1, 2, 3, 4, 5, 6]) {
-          if(count % 7 == i) {
-            data[i - 1].push(line);
-          }
-        }
-        if(count %7 == 0) {
-          data[6].push(line);
+  let data = [[], [], [], [], [], [], []];
+  let count = -6;
+  for (let line of lines) {
+    if(count > 0) {
+      for (let i of [1, 2, 3, 4, 5, 6]) {
+        if(count % 7 == i) {
+          data[i - 1].push(line);
         }
       }
-      count++;
-    }
-
-    let leaderboard = [];
-
-    let c = 0;
-    for( let i in data[0] ) {
-      let tmp = [];
-      for( let j in [1, 2, 3, 4, 5, 6, 7] ) {
-        tmp.push(data[j][c]);
+      if(count %7 == 0) {
+        data[6].push(line);
       }
-      leaderboard.push(tmp);
-      c++;
     }
-    fs.writeFile(output, JSON.stringify(leaderboard), (err) => { if (err) throw err; console.log(output); })
-  });
+    count++;
+  }
+
+  let leaderboard = [];
+
+  let c = 0;
+  for( let i in data[0] ) {
+    let tmp = [];
+    for( let j in [1, 2, 3, 4, 5, 6, 7] ) {
+      tmp.push(data[j][c]);
+    }
+    leaderboard.push(tmp);
+    c++;
+  }
+  fs.writeFile(output, JSON.stringify(leaderboard), (err) => { if (err) throw err; console.log(output); })
+
 };
 
 convert(process.argv[2])
