65d3226137c71cbf343ab4b939c3e429b1cfeac5
Sam Liu <ontouchstart@gmail.com>
2019-06-22 14:45:52 -0400

    top50 for week06

---
+++ bin/top50.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+const path = `data/leaderboard/week06/`
+const club_ids = JSON.parse(fs.readFileSync(`${path}/club_ids.json`, 'utf8'))
+
+const all_leaderboard = JSON.parse(fs.readFileSync(`${path}/leaderboard.json`, 'utf8'))
+
+let leaderboard = {}
+for( let id of club_ids) {
+  leaderboard[id] = all_leaderboard[id]
+}
+
+const weekdata = (leaderboard) => {
+  let output = [];
+  for (let id in leaderboard){
+    let club = leaderboard[id]
+    for (let c of club){
+      const mile = c[2]
+      if(mile) {
+      let entry = {
+        club_id : id,
+        name : c[1],
+        mile : (c[2] ? parseFloat(c[2])*0.621371 : 0),
+        count : c[3]
+      }
+      output.push(entry)
+      }
+    }
+  }
+  return output
+}
+
+const by_mile_and_name =  (a, b) => {  
+  if(b.mile < a.mile) {
+    return -1
+  }
+  if(b.mile > a.mile ) {
+    return 1
+  }
+  return b.name <= a.name ? 1 : -1
+}
+
+
+const uniq = (input) => {
+  let previous = input[0]
+  let output = [previous]
+  for (const i of input) {
+    if (i.name != previous.name || i.mile != previous.mile  ) {
+      previous = i
+      output.push(i)
+    }
+  }
+  return output
+}
+top50 = uniq(weekdata(leaderboard).sort(by_mile_and_name)).slice(0, 50)
+
+fs.writeFile(`${path}/top50.json`, JSON.stringify(top50, null, 2), (err) => { if (err) { throw err }; console.log(top50); })
+
