8c90839865769f8937b8830fdb68e72d239020b3
Sam Liu <ontouchstart@gmail.com>
2019-09-30 14:15:38 -0400

    skip 241951 Misty Mountain

--- bin/latest.js
+++ bin/latest.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const skipped = [
   '72363',   // ChiRunners 
   '128445',  // BURN
-  '204946'   // NewBee  (the last element has no comma)
+  '204946',   // NewBee
+  '241951'   // Misty Mountain (the last element has no comma)
 ];
 const latest = (id) => {
   if(id) {
